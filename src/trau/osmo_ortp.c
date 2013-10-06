e58d33153dd2bed3629b9a09fd6add58f296bd6a
Holger Hans Peter Freyther <holger@moiji-mobile.com>
2013-10-06 13:34:43 +0200

    ortp: Fix the version check for ORTP by not using the cached result
    The second check used the cached result for ORTP. Store the result
    in ORTP_VERSION and my system properly detects that it is running
    on < 0.21.
    
    The HAVE_ORTP_021 will always be defined so just use #if and not
    the #ifdef to fix the build.

--- src/trau/osmo_ortp.c
+++ src/trau/osmo_ortp.c
@@ -349,7 +349,7 @@ struct osmo_rtp_socket *osmo_rtp_socket_create(void *talloc_ctx, unsigned int fl
 int osmo_rtp_socket_bind(struct osmo_rtp_socket *rs, const char *ip, int port)
 {
 	int rc;
-#ifdef HAVE_ORTP_021
+#if HAVE_ORTP_021
 	rc = rtp_session_set_local_addr(rs->sess, ip, port, port+1);
 #else
 	rc = rtp_session_set_local_addr(rs->sess, ip, port);
