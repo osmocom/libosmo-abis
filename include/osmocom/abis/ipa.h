205e90314d28120ec6da4d82fc9e893130006d3f
Harald Welte <laforge@gnumonks.org>
2014-08-17 11:48:44 +0200

    ipa.h: Wrap function declarations to make them more readable
    purely cosmetic change.

--- include/osmocom/abis/ipa.h
+++ include/osmocom/abis/ipa.h
@@ -14,7 +14,11 @@ struct ipa_server_link {
 	void				*data;
 };
 
-struct ipa_server_link *ipa_server_link_create(void *ctx, struct e1inp_line *line, const char *addr, uint16_t port, int (*accept_cb)(struct ipa_server_link *link, int fd), void *data);
+struct ipa_server_link *
+ipa_server_link_create(void *ctx, struct e1inp_line *line, const char *addr,
+		       uint16_t port,
+		       int (*accept_cb)(struct ipa_server_link *link, int fd),
+		       void *data);
 void ipa_server_link_destroy(struct ipa_server_link *link);
 
 int ipa_server_link_open(struct ipa_server_link *link);
@@ -30,7 +34,11 @@ struct ipa_server_conn {
 	struct msgb			*pending_msg;
 };
 
-struct ipa_server_conn *ipa_server_conn_create(void *ctx, struct ipa_server_link *link, int fd, int (*cb)(struct ipa_server_conn *peer, struct msgb *msg), int (*closed_cb)(struct ipa_server_conn *peer), void *data);
+struct ipa_server_conn *
+ipa_server_conn_create(void *ctx, struct ipa_server_link *link, int fd,
+		       int (*cb)(struct ipa_server_conn *peer, struct msgb *msg),
+		       int (*closed_cb)(struct ipa_server_conn *peer),
+		       void *data);
 void ipa_server_conn_destroy(struct ipa_server_conn *peer);
 
 void ipa_server_conn_send(struct ipa_server_conn *peer, struct msgb *msg);
@@ -57,7 +65,13 @@ struct ipa_client_conn {
 	struct msgb			*pending_msg;
 };
 
-struct ipa_client_conn *ipa_client_conn_create(void *ctx, struct e1inp_ts *ts, int priv_nr, const char *addr, uint16_t port, void (*updown)(struct ipa_client_conn *link, int), int (*read_cb)(struct ipa_client_conn *link, struct msgb *msgb), int (*write_cb)(struct ipa_client_conn *link), void *data);
+struct ipa_client_conn *
+ipa_client_conn_create(void *ctx, struct e1inp_ts *ts, int priv_nr,
+			const char *addr, uint16_t port,
+			void (*updown)(struct ipa_client_conn *link, int),
+			int (*read_cb)(struct ipa_client_conn *link, struct msgb *msgb),
+			int (*write_cb)(struct ipa_client_conn *link),
+			void *data);
 void ipa_client_conn_destroy(struct ipa_client_conn *link);
 
 int ipa_client_conn_open(struct ipa_client_conn *link);
