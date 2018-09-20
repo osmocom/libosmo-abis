osmocom/libosmo-abis

commit b24efa551dc91e177c5cb8da674e9f8432d52dc9

Properly deal with sockaddr_un socket path length limitations.
When parsing the configuration, reject a socket path which
exceeds the maximum size supported by the operating system.

In unixsocket_line_update() stop copying the line's socket path to a
local buffer. The path will be copied again in osmo_sock_unix_init().

Both changes are portable; we don't assume any particular socket
path length since the size differs between implementations of Unix,
and we rely only on information from the generic sys/un.h header.

Change-Id: I36344805a825f5d0e0c9d218d438d8fd985ed9ca
Related: OS#2673

src/e1_input_vty.c
@@ -23,6 +23,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/un.h>
 
 #include <osmocom/core/linuxlist.h>
 #include <osmocom/core/talloc.h>
@@ -98,6 +99,14 @@ DEFUN(cfg_e1line_socket, cfg_e1_line_socket_cmd,
 {
 	struct e1inp_line *line;
 	int e1_nr = atoi(argv[0]);
+	struct sockaddr_un sun;
+
+	/* Don't exceed the maximum unix socket path length. See the unix(7) man page.*/
+	if (strlen(argv[1]) > sizeof(sun.sun_path)) {
+		vty_out(vty, "%% Socket path length exceeds %zd bytes: '%s'%s",
+			sizeof(sun.sun_path), argv[1], VTY_NEWLINE);
+		return CMD_WARNING;
+	}
 
 	line = e1inp_line_find(e1_nr);
 	if (!line) {

src/input/unixsocket.c
@@ -28,6 +28,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/socket.h>
+#include <sys/un.h>
 #include <limits.h>
 #include <string.h>
 
@@ -228,15 +229,10 @@ static void unixsocket_write_msg_lapd_cb(struct msgb *msg, void *cbdata)
 static int unixsocket_line_update(struct e1inp_line *line)
 {
 	struct unixsocket_line *config;
-	char sock_path[PATH_MAX];
+	char default_sock_path[sizeof(struct sockaddr_un) + 1]; /* see unix(7) man page */
+	const char *sock_path;
 	int ret = 0;
 	int i;
-
-	if (line->sock_path)
-		osmo_strlcpy(sock_path, line->sock_path, PATH_MAX);
-	else
-		sprintf(sock_path, "%s%d", UNIXSOCKET_SOCK_PATH_DEFAULT,
-			line->num);
 
 	LOGP(DLINP, LOGL_NOTICE, "line update (line=%p)\n", line);
 
@@ -255,6 +251,12 @@ static int unixsocket_line_update(struct e1inp_line *line)
 	config->fd.cb = unixsocket_cb;
 
 	/* Open unix domain socket */
+	if (line->sock_path == NULL) {
+		snprintf(default_sock_path, sizeof(default_sock_path), "%s%d",
+			 UNIXSOCKET_SOCK_PATH_DEFAULT, line->num);
+		sock_path = default_sock_path;
+	} else
+		sock_path = line->sock_path;
 	ret = osmo_sock_unix_init(SOCK_SEQPACKET, 0, sock_path,
 				  OSMO_SOCK_F_CONNECT);
 	if (ret < 0) {

commit 0d7d0b0a861c1abeb6a4852c914bc0a483dc9744

ensure unix socket paths are NUL-terminated for bind/connect
The unix(7) man page recommends that sun_path is NUL-terminated
when struct sockaddr_un is passed to a bind() or connect() call.
Non-NUL-terminated paths only need to be dealt with at the
receiving end of a UNIX domain socket.

Commit b24efa5 erroneously assumed otherwise.

Change-Id: I9beecfa500db75cb679b1edcc352c893bf098b13
Fixes: b24efa551dc91e177c5cb8da674e9f8432d52dc9
Related: OS#2673

src/e1_input_vty.c
@@ -101,10 +101,10 @@ DEFUN(cfg_e1line_socket, cfg_e1_line_socket_cmd,
 	int e1_nr = atoi(argv[0]);
 	struct sockaddr_un sun;
 
-	/* Don't exceed the maximum unix socket path length. See the unix(7) man page.*/
-	if (strlen(argv[1]) > sizeof(sun.sun_path)) {
+	/* Don't exceed the maximum unix socket path length, including a NUL byte. See the unix(7) man page.*/
+	if (strlen(argv[1]) > sizeof(sun.sun_path) - 1) {
 		vty_out(vty, "%% Socket path length exceeds %zd bytes: '%s'%s",
-			sizeof(sun.sun_path), argv[1], VTY_NEWLINE);
+			sizeof(sun.sun_path) - 1, argv[1], VTY_NEWLINE);
 		return CMD_WARNING;
 	}
 

src/input/unixsocket.c
@@ -229,7 +229,7 @@ static void unixsocket_write_msg_lapd_cb(struct msgb *msg, void *cbdata)
 static int unixsocket_line_update(struct e1inp_line *line)
 {
 	struct unixsocket_line *config;
-	char default_sock_path[sizeof(struct sockaddr_un) + 1]; /* see unix(7) man page */
+	struct sockaddr_un un;
 	const char *sock_path;
 	int ret = 0;
 	int i;
@@ -252,9 +252,17 @@ static int unixsocket_line_update(struct e1inp_line *line)
 
 	/* Open unix domain socket */
 	if (line->sock_path == NULL) {
-		snprintf(default_sock_path, sizeof(default_sock_path), "%s%d",
-			 UNIXSOCKET_SOCK_PATH_DEFAULT, line->num);
-		sock_path = default_sock_path;
+		ret = snprintf(un.sun_path, sizeof(un.sun_path), "%s%d",
+		    UNIXSOCKET_SOCK_PATH_DEFAULT, line->num);
+		if (ret == -1) {
+			LOGP(DLINP, LOGL_ERROR, "Cannot create default socket path: %s\n", strerror(errno));
+			return -errno;
+		} else if (ret >= sizeof(un.sun_path)) {
+			LOGP(DLINP, LOGL_ERROR, "Default socket path exceeds %zd bytes: %s%d\n",
+			     sizeof(un.sun_path), UNIXSOCKET_SOCK_PATH_DEFAULT, line->num);
+			return -ENOSPC;
+		}
+		sock_path = un.sun_path;
 	} else
 		sock_path = line->sock_path;
 	ret = osmo_sock_unix_init(SOCK_SEQPACKET, 0, sock_path,