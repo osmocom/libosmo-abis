osmocom/libosmo-abis

commit 4c57eef663e870247cb7a72130a50750317a44b9

fix strncpy bugs in ipaccess.c, ipa_proxy.c
Change-Id: Iad53b603521c0a8f4857bd87dca777ba8e875cde

src/input/ipaccess.c
@@ -602,19 +602,19 @@ ipa_bts_id_resp(const struct ipaccess_unit *dev, uint8_t *data, int len, int trx
 			break;
 		case IPAC_IDTAG_LOCATION1:
 			if (dev->location1)
-				strncpy(str, dev->location1, IPA_STRING_MAX);
+				osmo_strlcpy(str, dev->swversion, sizeof(str));
 			break;
 		case IPAC_IDTAG_LOCATION2:
 			if (dev->location2)
-				strncpy(str, dev->location2, IPA_STRING_MAX);
+				osmo_strlcpy(str, dev->swversion, sizeof(str));
 			break;
 		case IPAC_IDTAG_EQUIPVERS:
 			if (dev->equipvers)
-				strncpy(str, dev->equipvers, IPA_STRING_MAX);
+				osmo_strlcpy(str, dev->swversion, sizeof(str));
 			break;
 		case IPAC_IDTAG_SWVERSION:
 			if (dev->swversion)
-				strncpy(str, dev->swversion, IPA_STRING_MAX);
+				osmo_strlcpy(str, dev->swversion, sizeof(str));
 			break;
 		case IPAC_IDTAG_UNITNAME:
 			snprintf(str, sizeof(str),
@@ -626,7 +626,7 @@ ipa_bts_id_resp(const struct ipaccess_unit *dev, uint8_t *data, int len, int trx
 			break;
 		case IPAC_IDTAG_SERNR:
 			if (dev->serno)
-				strncpy(str, dev->serno, IPA_STRING_MAX);
+				osmo_strlcpy(str, dev->swversion, sizeof(str));
 			break;
 		default:
 			LOGP(DLINP, LOGL_NOTICE,
@@ -634,7 +634,6 @@ ipa_bts_id_resp(const struct ipaccess_unit *dev, uint8_t *data, int len, int trx
 			msgb_free(nmsg);
 			return NULL;
 		}
-		str[IPA_STRING_MAX-1] = '\0';
 
 		LOGP(DLINP, LOGL_INFO, " tag %d: %s\n", data[1], str);
 		tag = msgb_put(nmsg, 3 + strlen(str) + 1);

src/ipa_proxy.c
@@ -245,8 +245,7 @@ static int __ipa_instance_add(struct vty *vty, int argc, const char *argv[])
 			VTY_NEWLINE);
 		return CMD_WARNING;
 	}
-	strncpy(ipi->name, argv[0], IPA_INSTANCE_NAME);
-	ipi->name[IPA_INSTANCE_NAME - 1] = '\0';
+	osmo_strlcpy(ipi->name, argv[0], sizeof(ipi->name));
 	ipi->net.type = type;
 	ipi->net.addr = talloc_strdup(tall_ipa_proxy_ctx, argv[2]);
 	ipi->net.port = port;

commit 961776a2f97cb6d2877939f1cd1a21545ce8c57d

log IPA tags correctly
Due to apparent copy-paste errors, wrong values were being
logged for IPA tags in ipa_bts_id_resp().

Fixes: 4c57eef663e870247cb7a72130a50750317a44b9
Change-Id: I29a0401db0760219b9b9176709a88d589312261b
Related: OS#3355

src/input/ipaccess.c
@@ -603,15 +603,15 @@ ipa_bts_id_resp(const struct ipaccess_unit *dev, uint8_t *data, int len, int trx
 			break;
 		case IPAC_IDTAG_LOCATION1:
 			if (dev->location1)
-				osmo_strlcpy(str, dev->swversion, sizeof(str));
+				osmo_strlcpy(str, dev->location1, sizeof(str));
 			break;
 		case IPAC_IDTAG_LOCATION2:
 			if (dev->location2)
-				osmo_strlcpy(str, dev->swversion, sizeof(str));
+				osmo_strlcpy(str, dev->location2, sizeof(str));
 			break;
 		case IPAC_IDTAG_EQUIPVERS:
 			if (dev->equipvers)
-				osmo_strlcpy(str, dev->swversion, sizeof(str));
+				osmo_strlcpy(str, dev->equipvers, sizeof(str));
 			break;
 		case IPAC_IDTAG_SWVERSION:
 			if (dev->swversion)
@@ -627,7 +627,7 @@ ipa_bts_id_resp(const struct ipaccess_unit *dev, uint8_t *data, int len, int trx
 			break;
 		case IPAC_IDTAG_SERNR:
 			if (dev->serno)
-				osmo_strlcpy(str, dev->swversion, sizeof(str));
+				osmo_strlcpy(str, dev->serno, sizeof(str));
 			break;
 		default:
 			LOGP(DLINP, LOGL_NOTICE,