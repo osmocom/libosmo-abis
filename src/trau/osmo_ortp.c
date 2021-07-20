osmocom/libosmo-abis

commit 28eeb6bc9365bbe47125775fcb1f696317c22466

ortp: reset rtp session on SSRC changes
When libortp detects a timestamp jump, we resynchronize the session
in order to restore proper rtp frame reception. However, we do not
resynchronize on an SSRC change. An SSRC change usually indicates
the change of an RTP stream source, which is a much more profound
event than a timestamp change, so we should even reset the session
on SSRC changes.

Also, not resynchronizing the session on an SSRC change causes
problems when the jitter buffer feature is used. In those cases
it takes libortp a long time until it finally detects the timestamp
jump and the session resyhcornization happens late.

- reset the ession on SSRC changes by calling rtp_session_reset()

Change-Id: I8688a55cd20d0e14493c357db80754851e12f2fb
Related OS#3299

src/trau/osmo_ortp.c
@@ -114,7 +114,8 @@ static void ortp_sig_cb_ssrc(RtpSession *rs, void *data)
 	uint32_t ssrc = rtp_session_get_recv_ssrc(rs);
 
 	LOGP(DLMIB, LOGL_INFO,
-	     "osmo-ortp(%d): ssrc_changed to 0x%08x\n", port, ssrc);
+	     "osmo-ortp(%d): ssrc_changed to 0x%08x, resetting\n", port, ssrc);
+	rtp_session_reset(rs);
 }
 
 static void ortp_sig_cb_pt(RtpSession *rs, void *data)

commit 24e0217c2eca8e3197804a000c9b237daebc9f0c

osmo_ortp: Fix seqno reset to 0 upon ssrc_changed
It was spotted in an osmo-mgw instance receiving RTP from osmo-bts 1.2.3
that sometimes the seqno was reset to 0 (while tx_timestamp was kept
properly). This seems to be the only possible way osmo-bts can reset the
seqno to 0 (calling rtp_session_reset()), since osmo_rtp_socket_create
already sets it to other values and inside ortp rtp_session_reset() is
the only one setting it to 0.
If SSRC changes, we are fine resetting some Rx state, which is done by
calling rtp_session_resync. However, calling rtp_session_reset() is too
much, since it erases other stuff like Tx state, which sholdn't be
affected in this case, since we are still transmitting the same stream.

Related: OS#3104
Related: OS#3299
Related: SYS#5498
Fixes: 28eeb6bc9365bbe47125775fcb1f696317c22466

Change-Id: Ic6006b4991c85413b009b70f8f3a6f97c41990ec

src/trau/osmo_ortp.c
@@ -142,8 +142,8 @@ static void ortp_sig_cb_ssrc(RtpSession *rs, void *data)
 	uint32_t ssrc = rtp_session_get_recv_ssrc(rs);
 
 	LOGP(DLMIB, LOGL_INFO,
-	     "osmo-ortp(%d): ssrc_changed to 0x%08x, resetting\n", port, ssrc);
-	rtp_session_reset(rs);
+	     "osmo-ortp(%d): ssrc_changed to 0x%08x, resyncing\n", port, ssrc);
+	rtp_session_resync(rs);
 }
 
 static void ortp_sig_cb_pt(RtpSession *rs, void *data)