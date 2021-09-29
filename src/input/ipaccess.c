osmocom/libosmo-abis

commit d6216405b7363c94174a6d301b96f5ed4a8730ce

input: fix multiple BTS attached to single line scenario
With multiple BTS attached to a single line, we have to call
->line_update() multiple times. I broke this myself while avoiding
that A-bis over IP drivers bind to the socket several times.

To fix this situation, Harald prefers that this case is internally
handled by the ipaccess and hsl drivers by means of the driver_data
field in the e1inp_line structure.

Reported-by: Gus Bourg <gus@bourg.net>

src/input/ipaccess.c
@@ -813,9 +813,29 @@ static int ipaccess_bts_cb(struct ipa_client_link *link, struct msgb *msg)
 	return 0;
 }
 
+struct ipaccess_line {
+	int line_already_initialized;
+};
+
 static int ipaccess_line_update(struct e1inp_line *line)
 {
 	int ret = -ENOENT;
+	struct ipaccess_line *il;
+
+	if (!line->driver_data)
+		line->driver_data = talloc_zero(line, struct ipaccess_line);
+
+	if (!line->driver_data) {
+		LOGP(DLINP, LOGL_ERROR, "ipaccess: OOM in line update\n");
+		return -ENOMEM;
+	}
+	il = line->driver_data;
+
+	/* We only initialize this line once. */
+	if (il->line_already_initialized)
+		return 0;
+
+	il->line_already_initialized = 1;
 
 	switch(line->ops->cfg.ipa.role) {
 	case E1INP_LINE_R_BSC: {

src/e1_input.c
@@ -676,10 +676,6 @@ int e1inp_line_update(struct e1inp_line *line)
 
 	e1inp_line_get(line);
 
-	/* This line has been already initialized, skip this. */
-	if (line->refcnt > 2)
-		return 0;
-
 	if (line->driver && line->ops && line->driver->line_update) {
 		rc = line->driver->line_update(line);
 	} else

src/input/hsl.c
@@ -456,9 +456,29 @@ static int hsl_bts_connect(struct ipa_client_link *link)
 	return 0;
 }
 
+struct hsl_line {
+	int line_already_initialized;
+};
+
 static int hsl_line_update(struct e1inp_line *line)
 {
 	int ret = -ENOENT;
+	struct hsl_line *hl;
+
+	if (!line->driver_data)
+		line->driver_data = talloc_zero(line, struct hsl_line);
+
+	if (!line->driver_data) {
+		LOGP(DLINP, LOGL_NOTICE, "hsl: OOM in line update\n");
+		return -ENOMEM;
+	}
+	hl = line->driver_data;
+
+	/* We only initialize this line once. */
+	if (hl->line_already_initialized)
+		return 0;
+
+	hl->line_already_initialized = 1;
 
 	switch(line->ops->cfg.ipa.role) {
 	case E1INP_LINE_R_BSC:

commit d2d28d83a437f7478a4dfff0c6cae5305801b881

ipaccess: Allow reconfiguring the ipa line during line_update()
The e1inp_line_update() API is precisely aimed at that: tell that we
want to reconfigure some aspects of the e1 line. Hence, it makes no
sense to apply a no-op with the flag "line_already_initialized".
That flag was added a long time ago in order to avoid problems when
several BTS are attached to the same ipa server link. That case doesn't
apply to the BTS role, so we can simply recreate the ipa_cli_conn.

Fixes: d6216405b7363c94174a6d301b96f5ed4a8730ce
Change-Id: Iff8092f88726cf238dac2abb99e135ac5864272d

src/input/ipaccess.c
@@ -1005,6 +1005,7 @@ err:
 
 struct ipaccess_line {
 	bool line_already_initialized;
+	struct ipa_client_conn *ipa_cli;
 };
 
 static int ipaccess_line_update(struct e1inp_line *line)
@@ -1021,12 +1022,11 @@ static int ipaccess_line_update(struct e1inp_line *line)
 	}
 	il = line->driver_data;
 
-	/* We only initialize this line once. */
-	if (il->line_already_initialized)
-		return 0;
-
 	switch(line->ops->cfg.ipa.role) {
 	case E1INP_LINE_R_BSC: {
+		/* We only initialize this line once. */
+		if (il->line_already_initialized)
+			return 0;
 		struct ipa_server_link *oml_link, *rsl_link;
 		const char *ipa = e1inp_ipa_get_bind_addr();
 
@@ -1077,6 +1077,13 @@ static int ipaccess_line_update(struct e1inp_line *line)
 		     "OML connecting to %s:%u\n", line->ops->cfg.ipa.addr,
 		     IPA_TCP_PORT_OML);
 
+		/* Drop previous line */
+		if (il->ipa_cli) {
+			ipa_client_conn_close(il->ipa_cli);
+			ipa_client_conn_destroy(il->ipa_cli);
+			il->ipa_cli = NULL;
+		}
+
 		link = ipa_client_conn_create2(tall_ipa_ctx,
 					      e1inp_line_ipa_oml_ts(line),
 					      E1INP_SIGN_OML,
@@ -1104,6 +1111,7 @@ static int ipaccess_line_update(struct e1inp_line *line)
 
 		e1i_ts = e1inp_line_ipa_oml_ts(line);
 		ipaccess_bts_keepalive_fsm_alloc(e1i_ts, link, "oml_bts_to_bsc");
+		il->ipa_cli = link;
 		ret = 0;
 		break;
 	}