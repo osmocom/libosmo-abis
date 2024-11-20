fb1a37fd801439472778d2edadab066a2014b73e
Pau Espin Pedrol <pespin@sysmocom.de>
2024-11-20 17:32:22 +0100

    libosmo-abis depends on libosmo-netif
    Move the lapd-over-datagram-*.c examples from libosmo-netif to
    libosmo-abis, since those examples requrie code provided by
    libosmo-abis.
    
    libosmo-abis will be soon making use of libosmo-netif APIs such as those
    available in stream.h
    
    Depends: libosmo-netif.git Change-Id I13d6e88158f6d9ce017986283183ee9c2cc68cae
    Change-Id: I079dc3999de508301dd37ed03e399356a58d3cab

--- contrib/jenkins.sh
+++ contrib/jenkins.sh
@@ -19,12 +19,12 @@ mkdir "$deps" || true
 
 verify_value_string_arrays_are_terminated.py $(find . -name "*.[hc]")
 
-osmo-build-dep.sh libosmocore "" --disable-doxygen
-
 export PKG_CONFIG_PATH="$inst/lib/pkgconfig:$PKG_CONFIG_PATH"
 export LD_LIBRARY_PATH="$inst/lib"
 export PATH="$inst/bin:$PATH"
 
+osmo-build-dep.sh libosmocore "" --disable-doxygen
+osmo-build-dep.sh libosmo-netif "" --disable-doxygen
 osmo-build-dep.sh osmo-e1d
 
 set +x
